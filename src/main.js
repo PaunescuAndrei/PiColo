13a4fa0584d40343a46dc5b93db028842d28dca0
unknown <dazzle.x00@gmail.com>
2020-01-29 20:45:29 +0200

    Modified login

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
 var crypto = require('crypto');
+var formidable = require('formidable');
 
 var con = mysql.createConnection({
 	host     : 'localhost',
@@ -27,79 +28,72 @@ app.use("/", express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-app.get('/', function(request, response){
-    if(request.session.loggedin){
-        response.redirect('/dashboard');
-    } else {
-        response.sendFile(__dirname + "/public/login.html");
-    }
-});
 
 app.post('/auth', function(request, response){
-    var username = request.body.username;
-    var password = request.body.password;
-
-    if(username && password){
-        const hash = crypto.createHash('sha256').update(password);
-        password = hash.digest('base64');
-        hash.destroy();
-
-        if(request.body.login_button == ''){ // Login
-            con.query(selectUsrPwd, [username, password], function(err, result, field){
-                if(result.length > 0) {
-                    request.session.loggedin = true;
-                    request.session.username = username;
-                    request.session.user_id = result[0].id;
-                    response.redirect('/dashboard');
-                }else {
-                    response.redirect('/loginerror');
-                }			
-            });
-        } else { // Register
-            const values = [
-                [username, password]
-            ];
-            con.query(selectUsr, [username], function(err, result, field){
-                if(result.length > 0){
-                    response.redirect('/registererror');
-                } else {
-                    con.query(insertUsrPwd, [values], function(err, result, field){
-                        if(err){
-                            response.send('MYSQL Error');
+    
+    new formidable.IncomingForm().parse(request, (err, fields, files) => {
+        if (err) {
+            console.error('Error', err)
+            response.sendStatus(400);
+        } else {
+            var username = fields.username;
+            var password = fields.password;
+            var button = fields.button;
+            
+            if(username && password){
+                const hash = crypto.createHash('sha256').update(password);
+                password = hash.digest('base64');
+                hash.destroy();
+                
+                if(button == 'login'){ // Login
+                    con.query(selectUsrPwd, [username, password], function(err, result, field){
+                        if(result.length > 0) {
+                            request.session.loggedin = true;
+                            request.session.username = username;
+                            request.session.user_id = result[0].id;
+                            response.sendStatus(200);
+                        }else {
+                            response.sendStatus(401);
+                        }			
+                    });
+                } else { // Register
+                    const values = [
+                        [username, password]
+                    ];
+                    con.query(selectUsr, [username], function(err, result, field){
+                        if(result.length > 0){
+                            response.sendStatus(400);
+                        } else {
+                            con.query(insertUsrPwd, [values], function(err, result, field){
+                                if(err){
+                                    response.sendStatus(500);
+                                }
+                            });
                         }
                     });
                 }
-            });
+            }
         }
-    }
+    });
 });
 
 app.get('/dashboard', function(request, response){
-	if (request.session.loggedin) {
+    if (request.session.loggedin) {
         response.sendFile(__dirname + "/public/dashboard.html");
-        
 	} else {
-		response.sendFile(__dirname + "/public/logintoview.html");
+        response.sendFile(__dirname + "/public/logintoview.html");
 	}
 });
 
-app.get('/registererror', function(request, response){
+
+app.get('/dberror', function(request, response){
     if (request.session.loggedin) {
-		response.sendFile(__dirname + "/public/dashboard.html");
+        response.sendFile(__dirname + "/public/dashboard.html");
 	} else {
-		response.sendFile(__dirname + "/public/registererror.html");
+        response.sendFile(__dirname + "/public/loginerror.html");
 	}
 });
 
-app.get('/loginerror', function(request, response){
-    if (request.session.loggedin) {
-		response.sendFile(__dirname + "/public/dashboard.html");
-	} else {
-		response.sendFile(__dirname + "/public/loginerror.html");
-	}
-});
-
-// This do work
 // Make storage based on user / sw
 var multer  = require('multer')
 const storage = multer.diskStorage({
@@ -107,7 +101,7 @@ const storage = multer.diskStorage({
     filename: function (req, file, callback) {
         crypto.pseudoRandomBytes(16, function(err, raw) {
             if (err) return callback(err);
-                callback(null, raw.toString('hex') + path.extname(file.originalname));
+            callback(null, raw.toString('hex') + path.extname(file.originalname));
         });
     }
 });
@@ -116,9 +110,21 @@ app.post('/setParams', upload.single('watermark'), function(request, response, n
     const file = request.file;
     console.log(file);
     console.log(request.body);
-    return response.send({
-        success: true
-    })
+    response.send("salut");
+});
+
+app.get('/logout', function(request, response){
+    console.log(request.body);
+    request.session.destroy();
+    response.redirect('/');
+});
+
+app.get('/', function(request, response){
+    if(request.session.loggedin){
+        response.redirect('/dashboard');
+    } else {
+        response.sendFile(__dirname + "/public/login.html");
+    }
 });
 
 app.get('*', function(request, response){
